1db6db3d96c98589e3a7f529a415f260ad9bbc9c
Naveduran <naveduran@gmail.com>
2021-09-19 17:51:03 +0000

    [Add] jsonify response and make response to change the content type to json

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """Initialize flask functions"""
-
+from flask import jsonify, make_response
 from api.v1.views import app_views
 
 
 @app_views.route('/status', strict_slashes=False)
 def view_status():
     """Returns a JSON"""
-    return {"status": "OK"}
+    response = make_response(jsonify({"status": "OK"}))
+    response.headers["Content-Type"] = "application/json"
+    return response
