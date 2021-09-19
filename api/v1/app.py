ec47c3a281032a9c94220aad707caf217520d686
Naveduran <naveduran@gmail.com>
2021-09-19 17:24:08 +0000

    [Add] import flask and create the app

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,6 +4,9 @@
 from models import storage
 from api.v1.views import app_views
 from os import getenv
+from flask import Flask
+
+app = Flask(__name__)
 
 
 app.register_blueprint(app_views)
@@ -13,6 +16,7 @@ app.register_blueprint(app_views)
 def teardown(self):
     """Close process"""
     storage.close()
+
 
 if __name__ == "__main__":
     host = getenv('HBNB_API_HOST', default='0.0.0.0')
